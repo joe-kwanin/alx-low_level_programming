316edf5ed43900741d95a0433653d0f1681a8e41
joe-kwanin <kwame.kwanin@gmail.com>
2022-04-10 18:54:38 -0700

    Added 101-print_comb4 file

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,16 +11,16 @@ int main(void)
 
 	for (a = 48; a < 58; a++)
 	{
-		for (s = 48; s < 58; s++)
+		for (s = 49; s < 58; s++)
 		{
-			for (d = 48; d < 58; d++)
+			for (d = 50; d < 58; d++)
 			{
 				if (d != s && s > a)
 				{
 					putchar(a);
 					putchar(s);
 					putchar(d);
-					if(a != 55 || s != 56)
+					if (a != 55 || s != 56)
 					{
 						putchar(',');
 						putchar(' ');
