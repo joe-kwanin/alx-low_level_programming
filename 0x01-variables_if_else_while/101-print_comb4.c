2ebc69dd7539fc10f8fd65a919d64a7bf887c887
joe-kwanin <kwame.kwanin@gmail.com>
2022-04-10 18:52:25 -0700

    Added 101-print_comb4 file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible differernt combination of
+ * threee digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, s, d;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (s = 48; s < 58; s++)
+		{
+			for (d = 48; d < 58; d++)
+			{
+				if (d != s && s > a)
+				{
+					putchar(a);
+					putchar(s);
+					putchar(d);
+					if(a != 55 || s != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
